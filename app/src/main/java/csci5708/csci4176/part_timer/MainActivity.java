27538856e79debf782ea5d65fead4075f661147f
dineshkuncham <https://docs.google.com/document/d/1-lwQVum85PjAz41X_BBl51lpMl3MSyPUncJcJZUxQBs/edit>
2018-10-30 18:31:58 -0300

    added stats button in

--- app/src/main/java/csci5708/csci4176/part_timer/MainActivity.java
+++ app/src/main/java/csci5708/csci4176/part_timer/MainActivity.java
@@ -2,12 +2,16 @@ package csci5708.csci4176.part_timer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button stats;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        stats=findViewById(R.id.statsButton);
     }
 }
