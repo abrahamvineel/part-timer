51e32e5c0a08cd1141f235878b5ec89a1edcb02c
dineshkuncham <https://docs.google.com/document/d/1-lwQVum85PjAz41X_BBl51lpMl3MSyPUncJcJZUxQBs/edit>
2018-10-30 18:47:52 -0300

    added code to start Stats activity from main

--- app/src/main/java/csci5708/csci4176/part_timer/MainActivity.java
+++ app/src/main/java/csci5708/csci4176/part_timer/MainActivity.java
@@ -1,7 +1,9 @@
 package csci5708.csci4176.part_timer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         stats=findViewById(R.id.statsButton);
+        stats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), StatsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
