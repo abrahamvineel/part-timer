4b6efbdd8fe1b18ae1db35de5e60b713a5648fa4
vamsidwarsala <43305572+vamsidwarsala@users.noreply.github.com>
2018-11-20 11:11:53 -0400

    Added nav

--- app/src/main/java/csci5708/csci4176/part_timer/homepage.java
+++ app/src/main/java/csci5708/csci4176/part_timer/homepage.java
@@ -3,13 +3,12 @@ package csci5708.csci4176.part_timer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class homepage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_homepage);
     }
 }
+
