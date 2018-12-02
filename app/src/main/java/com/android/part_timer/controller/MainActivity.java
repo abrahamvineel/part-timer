4bfde8422bb1dcd7489146d79ebaede60ab94838
dineshkuncham <https://docs.google.com/document/d/1-lwQVum85PjAz41X_BBl51lpMl3MSyPUncJcJZUxQBs/edit>
2018-12-02 16:31:21 -0400

    removed background service code

--- app/src/main/java/com/android/part_timer/controller/MainActivity.java
+++ app/src/main/java/com/android/part_timer/controller/MainActivity.java
@@ -2,15 +2,9 @@ package com.android.part_timer.controller;
 
 import android.Manifest;
 import android.app.NotificationManager;
-import android.app.job.JobInfo;
-import android.app.job.JobScheduler;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
@@ -20,24 +14,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.android.part_timer.Constants;
-import com.android.part_timer.DialogAlert;
-import com.android.part_timer.MyBackgroundService;
 import com.android.part_timer.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = "MainActivity";
-    public static final int MY_BACKGROUND_JOB = 0;
     private final int MY_PERMISSION_ACCESS_FINE_LOCATION = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //  scheduleJob(this);
         BottomNavigationView bottom_nav = findViewById(R.id.bottom_nav);
         bottom_nav.setOnNavigationItemSelectedListener(navigation_listener);
         bottom_nav.setSelectedItemId(R.id.home);
@@ -59,27 +47,6 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_ACCESS_FINE_LOCATION);
         }
         return true;
-    }
-
-    public static void scheduleJob(Context context) {
-
-        JobScheduler js =
-                (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
-        JobInfo jobInfo;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            jobInfo = new JobInfo.Builder(
-                    MY_BACKGROUND_JOB,
-                    new ComponentName(context, MyBackgroundService.class))
-                    .setMinimumLatency(15 * 60 * 1000)
-                    .build();
-        } else {
-            jobInfo = new JobInfo.Builder(
-                    MY_BACKGROUND_JOB,
-                    new ComponentName(context, MyBackgroundService.class))
-                    .setPeriodic(15 * 60 * 1000)
-                    .build();
-        }
-        js.schedule(jobInfo);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navigation_listener =
